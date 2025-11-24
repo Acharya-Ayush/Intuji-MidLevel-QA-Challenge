b97dcdf54446cf1921ef71db202b1a5bbcd4299e
Ayush Acharya <49689971+Acharya-Ayush@users.noreply.github.com>
2025-11-24 16:56:47 +0545

    Merge pull request #2 from Acharya-Ayush/Setup
    tests

---
+++ pages/web_pages/home_page.py
@@ -0,0 +1,15 @@
+from pages.base import BasePage
+from setup.data_entry import base_url
+from selenium.webdriver.common.by import By
+
+class HomePage(BasePage):
+     #locators
+     login_nav = (By.XPATH, "//text()[contains(.,'Signup / Login')]")
+     
+     def __init__(self, driver, wait_time=20):
+        super().__init__(driver, wait_time)
+        
+     def goto_signup_page(self):
+          print("Going into login page")
+          self.navigate_to(base_url)
+          self.click_element(self.login_nav)
---
+++ setup/data_entry.py
@@ -0,0 +1,5 @@
+import os
+
+build_name = os.environ.get("BUILD_NAME", "Your Name")
+base_url = "https://automationexercise.com/" #dont change
+timezone = os.environ.get("TIMEZONE", "Kathmandu")
