bfa97b7f6a0ddc6ccc2183dcd550a54fd1c409d7
Ayush Acharya <ayushac9801@gmail.com>
2025-11-24 19:33:31 +0545

    First functional code.

--- pages/web_pages/home_page.py
+++ pages/web_pages/home_page.py
@@ -6,13 +6,16 @@ class HomePage(BasePage):
      #locators
      login_nav = (By.XPATH, "//*[contains(normalize-space(), 'Signup / Login')]")
      name_input_box = (By.XPATH, "//input[@data-qa=\"signup-name\"]")
+     visible_element_in_signup_page = (By.XPATH, "//h2[contains(text(),\"New User Signup!\")]")
      
      def __init__(self, driver, wait_time=20):
         super().__init__(driver, wait_time)
         
-     def goto_signup_page(self):
-          print("Going into login page")
-          self.navigate_to(base_url)
-          self.click_element(self.login_nav)
+     def goto_signup_page_and_signup(self):
+         print("Going into login page")
+         self.navigate_to(base_url)
+         self.navigate_to(url = "https://automationexercise.com/login")
+         self.wait_for_visibility(self.visible_element_in_signup_page)
+         self.insert_keys(self.name_input_box, text="Ayush")
           
           
--- setup/web_setup.py
+++ setup/web_setup.py
@@ -2,9 +2,17 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 
 def web_driver():
+    options = Options()
+    options.add_argument("--disable-logging")
+    options.add_argument("--log-level=3")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--no-sandbox")
+
     service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service)
+    driver = webdriver.Chrome(service=service, options=options)
     driver.maximize_window()
     return driver
