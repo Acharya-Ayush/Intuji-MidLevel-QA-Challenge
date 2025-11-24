4c1989eefea6f9559d9e61c5df5242422f089cfc
Ayush Acharya <ayushac9801@gmail.com>
2025-11-24 21:20:06 +0545

    SignUp process and Account existence Validation

--- pages/web_pages/home_page.py
+++ pages/web_pages/home_page.py
@@ -1,12 +1,44 @@
 from pages.base import BasePage
 from setup.data_entry import base_url
 from selenium.webdriver.common.by import By
+import os
+from dotenv import load_dotenv
+import time
+from selenium.common.exceptions import TimeoutException
 
 class HomePage(BasePage):
      #locators
      login_nav = (By.XPATH, "//*[contains(normalize-space(), 'Signup / Login')]")
      name_input_box = (By.XPATH, "//input[@data-qa=\"signup-name\"]")
+     email_input_box = (By.XPATH, "//input[@data-qa=\"signup-email\"]")
+     signup_button = (By.XPATH, "//button[@data-qa=\"signup-button\"]")
+     password_field = (By.XPATH, "//input[@data-qa=\"password\"]")
+     day_of_birth = (By.XPATH, "//select[@data-qa=\"days\"]")
+     month_of_birth = (By.XPATH, "//select[@data-qa=\"months\"]")
+     year_of_birth = (By.XPATH, "//select[@data-qa=\"years\"]")
+     first_name = (By.XPATH, "//input[@data-qa=\"first_name\"]")
+     last_name = (By.XPATH, "//input[@data-qa=\"last_name\"]")
+     company = (By.XPATH, "//input[@data-qa=\"company\"]")
+     address = (By.XPATH, "//input[@data-qa=\"address\"]")
+     country = (By.XPATH, "//select[@data-qa=\"country\"]")
+     state = (By.XPATH, "//input[@data-qa=\"state\"]")
+     city = (By.XPATH, "//input[@data-qa=\"city\"]")
+     zipcode = (By.XPATH, "//input[@data-qa=\"zipcode\"]")
+     mobile_number = (By.XPATH, "//input[@data-qa=\"mobile_number\"]")  
+     create_account_btn = (By.XPATH, "//button[@data-qa=\"create-account\"]")
+     verification_account_created = (By.XPATH, "//b[contains(text(),\"Account Created\")]")
+     verification_for_taken_email = (By.XPATH, "//p[contains(text(),'Email Address already exist!')]")
+     
+     #Visible element check after loading the URL.
      visible_element_in_signup_page = (By.XPATH, "//h2[contains(text(),\"New User Signup!\")]")
+     visible_element_in_signup_page_after_clicking_signup_button = (By.XPATH, "//b[contains(text(),\"Enter Account Information\")]")     
+     #load credentials from .env file
+     load_dotenv()
+     
+     USERNAME = os.getenv("USERNAME")
+     EMAIL = os.getenv("EMAIL")
+     PASSWORD = os.getenv("PASSWORD")
+     
      
      def __init__(self, driver, wait_time=20):
         super().__init__(driver, wait_time)
@@ -16,6 +48,43 @@ class HomePage(BasePage):
          self.navigate_to(base_url)
          self.navigate_to(url = "https://automationexercise.com/login")
          self.wait_for_visibility(self.visible_element_in_signup_page)
-         self.insert_keys(self.name_input_box, text="Ayush")
+         self.insert_keys(self.name_input_box, text=self.USERNAME)
+         self.insert_keys(self.email_input_box, text=self.EMAIL)
+         self.click_element(self.signup_button) 
+         try:
+            self.wait_for_visibility(self.verification_for_taken_email, wait_time=3)
+            print("The account already exists.")
+         except TimeoutException:   
+            self.wait_for_visibility(self.visible_element_in_signup_page_after_clicking_signup_button)
+            print("Went to Signup Page.")
+            print("Filling PAssword.")
+            self.insert_keys(self.password_field, text=self.PASSWORD)
+            print("Filling birth date")
+            self.select_from_dropdown(self.day_of_birth, text='5')
+            print("Filling birth month")
+            self.select_from_dropdown(self.month_of_birth, text='January')
+            print("Filling birth year")
+            self.select_from_dropdown(self.year_of_birth, text='1999')
+            print("Filling name, last name, company, Address, Country, state, City, ZIP, and Mobile Number.")
+            self.insert_keys(self.first_name, text = "TestUser2323")
+            self.insert_keys(self.last_name, text="TestUser3232")
+            self.insert_keys(self.company, text="INTUJI")
+            self.insert_keys(self.address, text="123 Main Street, Los Angeles, CA 90011")
+            self.select_from_dropdown(self.country, text="United States")
+            self.insert_keys(self.state, text="California")
+            self.insert_keys(self.city, text="Los Angeles")
+            self.insert_keys(self.zipcode, text="90011")
+            self.insert_keys(self.mobile_number, text="+19898767654")
+            self.click_element(self.create_account_btn)
+            # The code execution time is really fast so, need to implement break (This is not explicit wait.)
+            time.sleep(2)
+            self.wait_for_visibility(self.verification_account_created)
+         print("SignUp process completed.")
+         
+         
+         
+         
+         
+         
           
           
--- tests/test_signup.py
+++ tests/test_signup.py
@@ -6,5 +6,5 @@ from pages.web_pages.home_page import HomePage
 def test_signup():
      driver = web_driver()
      signup_page = HomePage(driver)
-     signup_page.goto_signup_page()
+     signup_page.goto_signup_page_and_signup()
      
