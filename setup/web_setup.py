71b95c56b7a497815e57b9e8f24dbb5cdef24fb7
Ayush Acharya <49689971+Acharya-Ayush@users.noreply.github.com>
2025-11-24 18:54:29 +0545

    Setup (#3)
    * improvements on setup
    
    * Adding signup
    
    * all chages
    
    * With test cases
    
    * Setup to run locally

--- setup/web_setup.py
+++ setup/web_setup.py
@@ -1,29 +1,11 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options as ChromeOptions
-from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium_tests.setup.data_entry import *
+import os
 
 
+def web_driver():
+    os.environ['WDM_LOCAL'] = '1'
+    os.environ['WDM_DIR'] = './drivers'
 
-def web_driver(test_name, build_name=build_name):
-    options = ChromeOptions()
-    options.browser_version = 'latest'
-    options.platform_name = 'Windows 10'
-    sauce_options = {}
-    sauce_options['username'] = saucelabs_username
-    sauce_options['accessKey'] = saucelabs_key
-    sauce_options['build'] = build_name
-    sauce_options['screenResolution'] = '1920x1080'
-    sauce_options['name'] = test_name
-    sauce_options['extendedDebugging'] = True
-    sauce_options['pageLoadStrategy'] = 'none' # or eager
-    #sauce_options['capturePerformance'] = True
-    sauce_options['timeZone'] = timezone
-    options.set_capability('sauce:options', sauce_options)
-
-    url = "https://ondemand.eu-central-1.saucelabs.com:443/wd/hub"
-    driver = webdriver.Remote(command_executor=url, options=options)
-    driver.maximize_window()
-
+    driver = webdriver.Chrome(ChromeDriverManager().install())
     return driver
